a3252129d240f6a2b1260d1991c966e03cb0c361
Foorogrammer <lleellee013@gmail.com>
2025-04-01 00:23:57 +0900

    예제 커밋

---
+++ src/main/java/resilience/emailservice/exception/InvalidEmailException.java
@@ -0,0 +1,7 @@
+package resilience.emailservice.exception;
+
+public class InvalidEmailException extends RuntimeException {
+    public InvalidEmailException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/resilience/emailservice/mail/MailController.java
@@ -0,0 +1,28 @@
+package resilience.emailservice.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mail")
+public class MailController {
+
+    @Autowired
+    private MailSenderService mailSenderService;
+
+    // http://localhost:8081/mail/send 로 요청
+    // POST 요청으로 email을 JSON 형식으로 받습니다.
+    @PostMapping("/send")
+    public String sendMail(@RequestBody EmailRequest emailRequest) {
+        mailSenderService.sendEmail(emailRequest.getEmail());
+        return "메일 전송 요청이 접수되었습니다.";
+    }
+
+    // AOP와 Self Invocation에 대해
+    @PostMapping("/send-for-aop-demo")
+    public String sendMailAopDemo(@RequestBody EmailRequest emailRequest) {
+        // sendEmail이 아니라 sendEmailForAopDemo 호출
+        mailSenderService.sendEmailForAopDemo(emailRequest.getEmail());
+        return "메일 전송 요청이 접수되었습니다.";
+    }
+}
